35dac6d1e4e56dc8298f2d279bdb2803c417aae1
WickedWhisperer <humood809@gmail.com>
2026-03-24 20:03:56 +0500

    Add video download tracking functionality

---
+++ core/tracker.py
@@ -0,0 +1,14 @@
+import os
+
+TRACK_FILE = "archive/downloaded.txt"
+
+def is_downloaded(video_id):
+    if not os.path.exists(TRACK_FILE):
+        return False
+
+    with open(TRACK_FILE, "r") as f:
+        return video_id in f.read()
+
+def mark_downloaded(video_id):
+    with open(TRACK_FILE, "a") as f:
+        f.write(video_id + "\n")
