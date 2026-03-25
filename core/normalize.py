63e09b0cf61cfc4103b0d41dd3d0ed614743141e
WickedWhisperer <humood809@gmail.com>
2026-03-25 13:49:00 +0500

    Refactor normalize function to structure video data

--- core/normalize.py
+++ core/normalize.py
@@ -1,3 +1,23 @@
-def normalize(video):
-    # return standardized structure
-    return video
+def normalize(item):
+    return {
+        "id": item.get("id"),
+        "author": item.get("authorMeta", {}).get("name"),
+        "caption": item.get("text"),
+        "created_at": item.get("createTimeISO"),
+        "url": item.get("webVideoUrl"),
+        "stats": {
+            "likes": item.get("diggCount"),
+            "shares": item.get("shareCount"),
+            "plays": item.get("playCount"),
+            "comments": item.get("commentCount"),
+            "saves": item.get("collectCount"),
+        },
+        "music": {
+            "name": item.get("musicMeta", {}).get("musicName"),
+            "author": item.get("musicMeta", {}).get("musicAuthor"),
+            "is_original": item.get("musicMeta", {}).get("musicOriginal"),
+        },
+        "duration": item.get("videoMeta", {}).get("duration"),
+        "platform": "tiktok",
+        "raw": item,
+    }
