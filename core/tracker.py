0aa2bec8f98d7d8d4b341fa79c10aded840a5ecf
WickedWhisperer <humood809@gmail.com>
2026-03-27 17:21:44 +0500

    Update tracker.py

--- core/tracker.py
+++ core/tracker.py
@@ -1,21 +1,24 @@
 import os
 
-TRACK_FILE = "archive/meta/downloaded.txt"
+TRACK_FILE = "archive/system/downloaded.txt"
 
 
-def _ensure_file():
+def ensure_file():
     os.makedirs(os.path.dirname(TRACK_FILE), exist_ok=True)
     if not os.path.exists(TRACK_FILE):
-        open(TRACK_FILE, "w").close()
+        with open(TRACK_FILE, "w"):
+            pass
 
 
 def is_downloaded(video_id):
-    _ensure_file()
+    ensure_file()
+
     with open(TRACK_FILE, "r") as f:
         return video_id in f.read()
 
 
 def mark_downloaded(video_id):
-    _ensure_file()
+    ensure_file()
+
     with open(TRACK_FILE, "a") as f:
         f.write(video_id + "\n")
