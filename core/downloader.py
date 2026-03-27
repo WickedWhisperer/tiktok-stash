502fd3d81a3208fbe3354f2d6ad62935590133ef
WickedWhisperer <humood809@gmail.com>
2026-03-27 17:48:54 +0500

    Update downloader.py

--- core/downloader.py
+++ core/downloader.py
@@ -2,17 +2,14 @@ import subprocess
 
 
 def download_video(url, output_path):
-    try:
-        command = [
+    subprocess.run(
+        [
             "yt-dlp",
-            "-o", output_path,
-            "-f", "mp4",
+            "-o",
+            output_path,
+            "-f",
+            "mp4",
             url
-        ]
-
-        result = subprocess.run(command, capture_output=True)
-
-        return result.returncode == 0
-
-    except Exception:
-        return False
+        ],
+        check=True
+    )
